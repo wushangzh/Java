64a0c8d54b3a3e337952bc5af0281803f948d432
wushangzh <126957096+wushangzh@users.noreply.github.com>
2023-03-06 18:37:25 +0800

    a new file again :)

---
+++ input.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class input {
+    public static void main(String[] args){
+        String answerString = JOptionPane.showInputDialog("太阳系有几个行星？");
+        String replyString;
+        int ans = Integer.parseInt(answerString);
+        if (ans==8){
+            replyString = "正确!";
+        }else{
+            replyString = "错了!!!\n";
+            replyString += "正确答案为： " + ans;
+        }
+        JOptionPane.showMessageDialog(null, replyString);
+    }
+}
